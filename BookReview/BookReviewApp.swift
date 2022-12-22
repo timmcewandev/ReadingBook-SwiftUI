41e278899d4071434473ee665e720110c3f43f9f
Tim McEwan <timmcewandev@gmail.com>
2022-12-22 09:40:29 -0700

    Initial Commit

---
+++ BookReview/BookReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReviewApp.swift
+//  BookReview
+//
+//  Created by Tim McEwan on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BookReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookReview
+//
+//  Created by Tim McEwan on 12/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worl!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
